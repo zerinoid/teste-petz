66c93ea0ef63762e955e921ec5ef6674966ca1be
zerinoid <leo.zerino@gmail.com>
2020-12-15 19:00:20 -0300

    fetch da api externa

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+export async function getStaticProps() {
+    
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const posts = await res.json()
+
+    return {
+        props: {
+            posts,
+        },
+    }
+}
+
 export default function HomePage() {
     return <div>Welcome to Next.js!</div>
 }
