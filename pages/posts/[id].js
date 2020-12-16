d8826e2c22a35cbd252a12bbe4ea5596a2b079d0
zerinoid <leo.zerino@gmail.com>
2020-12-15 22:01:04 -0300

    funções de requisição estática nextjs

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,20 +1,39 @@
 import Layout from "../../components/layout"
 import Head from "next/head"
 
-export default function Post({ postData }) {
+export async function getStaticPaths() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const posts = await res.json()
+    const paths = posts.map(post => ({
+        params: { id: post.id }
+    }))
+    return { paths, fallback: false }
+}
+
+export async function getStaticProps({ params }) {
+    // params contains the post `id`.
+    // If the route is like /posts/1, then params.id is 1
+    const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${params.id}`
+    )
+    const post = await res.json()
+
+    // Pass post data to the page via props
+    return { props: { post } }
+}
+
+export default function Post({ post }) {
     return (
         <Layout>
             <Head>
-                <title>{postData.title}</title>
+                <title>{post.title}</title>
             </Head>
             <article>
-                <h1>{postData.title}</h1>
+                <h1>{post.title}</h1>
                 {/* <div> */}
                 {/*     <Date dateString={postData.date} /> */}
                 {/* </div> */}
-                <div
-                    dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
-                />
+                <div>{post.body}</div>
             </article>
         </Layout>
     )
